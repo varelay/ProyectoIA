56f0710147b8c47b8eca1d24bbe77733a3597275
varelay <89622998+varelay@users.noreply.github.com>
2022-08-02 18:17:10 -0500

    Update conexion.php

--- bot/conexion.php
+++ bot/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = new mysqli("ls-09e393cc458d8c9984b7ce26cc0c199c2bf93ce5.cfv20klo2ytv.us-east-1.rds.amazonaws.com","dbmasteruser","n.cf^AgcyiETV(?t==*1}6F[Ky`uwV$G","sis_venta");
+$conexion = new mysqli("ls-09e393cc458d8c9984b7ce26cc0c199c2bf93ce5.cfv20klo2ytv.us-east-1.rds.amazonaws.com","dbmasteruser","SoyForaneo27","sis_venta");
 
 if ($conex){
     echo("Si hay conexión");
